221aef34887680e03023d029fde455b4300df131
Ziwen Yu <ziwenyu83@gmail.com>
2017-01-27 10:48:06 -0500

    Create Non_StationaryInterMFunctions.R

---
+++ General-Functions/Non_StationaryInterMFunctions.R
@@ -0,0 +1,44 @@
+
+#Pickup the nearest months
+NearestMonths<-function(x,n) {
+  #x is the centered month, n is the number of months
+  result=c((x-n %/% 2):(x+n %/% 2))
+  result[which(result>12)]=result[which(result>12)]-11
+  result[which(result<1)]=result[which(result<1)]+12
+  return(result)
+}
+
+#Pickup the nearest numbers
+FindKnearest<- function(x,n) {
+  #x is the centered value, n is the vector
+  n=abs(n-x)
+  r1=rank(n,ties.method= "first")
+  k=round(sqrt(length(n)))
+  return(which(r1<=k))
+}
+
+#update current month, current month Temperature
+UpdateCurMon_MonT <- function(x,MonTProj,GCM) {
+  #x is the current Date
+  CurMon<<-month(x)
+  CurMonT<<-MonTProj %>% 
+    gather(GCM,MonT,BCCR:UKMO) %>% 
+    filter(year(Date)==year(x), month(Date)==month(x),GCM=='BCCR') %>% 
+    .$MonT
+}
+
+
+#Round time to each hour
+Round_hour=function(Datetime)
+{
+  hrs=difftime(Datetime,
+             as.Date(Datetime),
+             units='hour') %>% 
+    round
+  
+  
+  return(as.POSIXct((as.Date(Datetime)+hours(hrs)), 
+                    format = "%Y-%m-%d %H:%M:%S")
+  )
+  
+}
