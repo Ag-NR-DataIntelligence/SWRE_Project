4cb9589ec7a635df7e023af124025d481ceee64a
Ziwen Yu <ziwenyu83@gmail.com>
2017-02-07 14:21:32 -0500

    Update ReadNYCClimateData.R

--- Non_stationaryGenerator/DataLoad/ReadNYCClimateData.R
+++ Non_stationaryGenerator/DataLoad/ReadNYCClimateData.R
@@ -3,7 +3,7 @@ source('https://raw.githubusercontent.com/ZyuAFD/SWRE_Project/master/Non_station
 library(data.table)
 library(lubridate)
 ###  Load Historical data -----------
-Path='Y:\\Research\\Precipitation analysis\\Data\\'
+Path='\\\\SWV.cae.drexel.edu\\ziwen\\Research\\Precipitation analysis\\Data\\'
 
 #### NYC LGA data  
 NYC_LGA_Clim=fread(paste0(Path,'NYC\\La Gadia\\Climate\\Data.txt'),
