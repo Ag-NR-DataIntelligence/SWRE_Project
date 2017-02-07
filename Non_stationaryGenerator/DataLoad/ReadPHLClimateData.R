be9cac4f57afc5dc78c48cba4d6929d89db3ebe7
Ziwen Yu <ziwenyu83@gmail.com>
2017-02-07 14:21:48 -0500

    Update ReadPHLClimateData.R

--- Non_stationaryGenerator/DataLoad/ReadPHLClimateData.R
+++ Non_stationaryGenerator/DataLoad/ReadPHLClimateData.R
@@ -4,7 +4,7 @@ source('https://raw.githubusercontent.com/ZyuAFD/SWRE_Project/master/Non_station
 library(data.table)
 library(lubridate)
 ###  Load Historical data -----------
-Path='Y:\\Research\\Precipitation analysis\\Data\\'
+Path='\\\\SWV.cae.drexel.edu\\ziwen\\Research\\Precipitation analysis\\Data\\'
 
 #### PHL data  
 PHL_Clim=fread(paste0(Path,'PHL\\Air Pressure & Temp & Humidity\\9901356023046dat.txt'),
