e88f2376f9b3e606cde17d67244f0d644e9408ab
Ziwen Yu <ziwenyu83@gmail.com>
2017-03-03 15:29:07 -0500

    Update Preciptation Event Separation.r

--- General-Functions/Preciptation Event Separation.r
+++ General-Functions/Preciptation Event Separation.r
@@ -16,5 +16,7 @@ Precip_Evt_Sep= function(dt)
   mutate(Evt_lab=cumsum(Evt_lab)) %>% 
   mutate(Evt_lab=ifelse(Evt_lab %% 2==0 & lag(Evt_lab)<Evt_lab,Evt_lab-1,Evt_lab)) %>% 
   mutate(Evt_lab=ifelse(Evt_lab %% 2==0,0,(Evt_lab+1) %/% 2)) %>%
+  select(Time,Rain,Evt_lab) %>% 
+        mutate(Evt_lab=ifelse(is.na(Evt_lab),0,Evt_lab)) %>% 
     return
 }
