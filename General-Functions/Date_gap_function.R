0321794031a236ee195aa6b285f41dbcb98f1301
Ziwen Yu <ziwenyu83@gmail.com>
2018-05-30 10:14:27 -0400

    Update Date_gap_function.R

--- General-Functions/Date_gap_function.R
+++ General-Functions/Date_gap_function.R
@@ -1,7 +1,11 @@
-Date_gap=function(Dt, SynDt,unit="day")
+SyncDate_gap=function(Dt, SynDt,unit="day")
 {
    interval(Dt,SynDt) %>% 
-        as.period() %>% 
-        as.numeric(unit=unit) %>% 
+        as.period()  %>% 
+        {
+            gap=.
+            (gap-years(as.numeric(gap,unit="year")%/%1)) %>% 
+                as.numeric(unit=unit)
+        }  %>% 
         return
 }
