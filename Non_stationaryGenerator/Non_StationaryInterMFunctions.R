089fd69dbe9df686130450075012387a3e596aa1
Ziwen Yu <ziwenyu83@gmail.com>
2018-06-05 09:46:56 -0400

    Add SyncDate_gap function

--- Non_stationaryGenerator/Non_StationaryInterMFunctions.R
+++ Non_stationaryGenerator/Non_StationaryInterMFunctions.R
@@ -42,3 +42,16 @@ Round_hour=function(Datetime)
   )
   
 }
+
+# function to find the day gap getween two synthetic dates
+SyncDate_gap=function(Dt, SynDt,unit="day")
+{
+    interval(Dt,SynDt) %>% 
+        as.period()  %>% 
+        {
+            gap=.
+            (gap-years(as.numeric(gap,unit="year")%/%1)) %>% 
+                as.numeric(unit=unit)
+        }  %>% 
+        return
+}
