6adc9b9aa4a3828b2d7a7741bd4b3ed90b72b268
Ziwen Yu <ziwenyu83@gmail.com>
2018-06-05 09:48:35 -0400

    Add synthetic data generation code

---
+++ Non_stationaryGenerator/Code to generate and save synthetic data.R
@@ -0,0 +1,23 @@
+
+source('https://raw.githubusercontent.com/ZyuAFD/SWRE_Project/master/Non_stationaryGenerator/DataLoad/DataPreparation.R')
+source('https://raw.githubusercontent.com/ZyuAFD/SWRE_Project/master/Non_stationaryGenerator/Non_StationaryInterMFunctions.R')
+source('https://raw.githubusercontent.com/ZyuAFD/SWRE_Project/master/Non_stationaryGenerator/Generator.R')
+
+setwd("Z:\\Working Publications\\Ziwen\\Climate Change\\Code\\")
+MonthT=MonthT_all %>% filter(Loc=="NYC",Emission=="A2")
+
+library(glue)
+# GCM models to simulate "IPSL","MIROC","MIUB","GFDL"
+set.seed(11)
+for (j in 1:5)
+{
+    SyncPrecip=SyncP_Generate(GCM = 'MIROC') 
+    SyncPrecip[[1]] %>% mutate(Runid=j)
+    SyncPrecip[[2]] %>% mutate(Runid=j)
+    
+    write_rds(SyncPrecip,path=glue("Data\\SynDt{j}.rds"),compress="gz")
+    Sys.sleep(0.1)
+    print(j)
+}
+
+
