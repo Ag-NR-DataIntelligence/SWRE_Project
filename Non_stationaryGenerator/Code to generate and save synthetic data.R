48bf8bd1bae07236f494da9189fe870bbee7bdc0
Ziwen Yu <ziwenyu83@gmail.com>
2018-07-13 12:05:37 -0400

    Change compress type

--- Non_stationaryGenerator/Code to generate and save synthetic data.R
+++ Non_stationaryGenerator/Code to generate and save synthetic data.R
@@ -3,7 +3,7 @@ source('https://raw.githubusercontent.com/ZyuAFD/SWRE_Project/master/Non_station
 source('https://raw.githubusercontent.com/ZyuAFD/SWRE_Project/master/Non_stationaryGenerator/Non_StationaryInterMFunctions.R')
 source('https://raw.githubusercontent.com/ZyuAFD/SWRE_Project/master/Non_stationaryGenerator/Generator.R')
 
-setwd("Z:\\Working Publications\\Ziwen\\Climate Change\\Code\\")
+setwd("\\\\swv.cae.drexel.edu\\group\\Working Publications\\Ziwen\\Climate Change\\Code\\")
 MonthT=MonthT_all %>% filter(Loc=="NYC",Emission=="A2")
 
 library(glue)
@@ -12,12 +12,11 @@ set.seed(11)
 for (j in 1:100)
 {
     SyncPrecip=SyncP_Generate(GCM = 'MIROC',TempWidth=1.5) 
-    SyncPrecip[[1]] %>% mutate(Runid=j)
-    SyncPrecip[[2]] %>% mutate(Runid=j)
     
-    write_rds(SyncPrecip,path=glue("Data\\SynDt{j}.rds"),compress="gz")
+    write_rds(SyncPrecip,path=glue("Data\\SynDt{j}.rds"),compress="xz")
     Sys.sleep(0.1)
     print(j)
 }
+
 
 
